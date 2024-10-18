f2fcf041711f56c7d7280b935498291388d8c7de
devCalf <caique.rosa@epiousion.com.br>
2024-10-18 13:33:29 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="app">Tela Principal</div>
+</template>
+
+<style>
+.app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+}
+</style>
